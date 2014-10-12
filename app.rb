83b3c515b6ff406bb19261b48164a54db6c73e25
Tejas Manohar <me@tejasmanohar.com>
2014-10-12 12:12:53 -0500

    search backend complete

--- app.rb
+++ app.rb
@@ -1,24 +1,25 @@
+# require necessary gems
 require 'sinatra'
 require 'json'
-
 require 'pry' if Sinatra::Base.development?
 
-require 'bundler'
-Bundler.require
-
-def get_definitions(word)
+# search quizlet sets for word's definition
+def get_entries(word)
   uri = URI("https://api.quizlet.com/2.0/search/definitions")
   params = { :client_id => ENV['KEY'], :q => word }
   uri.query = URI.encode_www_form(params)
-  JSON.parse(Net::HTTP.get_response(uri).body)
+  JSON.parse(Net::HTTP.get_response(uri).body)['official_definitions'][0..2]
 end
 
+# display homepage
 get '/' do
   erb :index
 end
 
+# parse and display results from search
 get '/search' do
   word = params[:q]
-  results = get_definitions word
-  arr = result['official_definitions'][0..3]['word'] + result['user_definitions'][0..3]['word']
+  @results = get_entries word
+  puts @results
+  erb :search
 end
