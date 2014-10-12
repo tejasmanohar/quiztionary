8ae9606aafe487dc6902a9826f18ef067f7034c1
Tejas Manohar <me@tejasmanohar.com>
2014-10-11 22:41:26 -0500

    lets finish at codeday

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 require 'json'
 
+require 'pry' if Sinatra::Base.development?
+
 require 'bundler'
 Bundler.require
 
+def get_definitions(word)
+  uri = URI("https://api.quizlet.com/2.0/search/definitions")
+  params = { :client_id => ENV['KEY'], :q => word }
+  uri.query = URI.encode_www_form(params)
+  JSON.parse(Net::HTTP.get_response(uri).body)
+end
+
 get '/' do
   erb :index
+end
+
+get '/search' do
+  word = params[:q]
+  results = get_definitions word
+  arr = result['official_definitions'][0..3]['word'] + result['user_definitions'][0..3]['word']
 end
